23b76531b5adf78882830e4b915e122bdd0fbd47
rookie shinichi <shinicho@ogaki-tv.ne.jp>
2026-06-16 16:02:20 +0900

    cssファイル(plant-detail)修正

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,25 @@
-const btn = document.getElementById('toggleBtn');
-const form = document.getElementById('formContent');
+document.addEventListener("DOMContentLoaded", () => {
+    // HTMLの読み込み完了後に要素を取得する
+    const btn = document.getElementById('toggleBtn');
+    const form = document.getElementById('formContent');
 
-btn.addEventListener('click', () => {
-    if (form.style.display === 'block') {
-        form.style.display = 'none';
-    } else {
-        form.style.display = 'block';
+    // form,btnが存在することを確認してから処理する
+    if (form && btn) {
+        // 初期状態のチェック（非表示でなければ、非表示にしてボタン文字を変える
+        if (form.style.display !== 'none') {
+            alert("フォームが開いています");
+            form.style.display = 'none';
+            btn.textContent = "作業フォームを開く";
+        }
     }
+
+    btn.addEventListener('click', () => {
+        if (form.style.display === 'block') {
+            form.style.display = 'none';
+            btn.textContent = "フォームを開く";
+        } else {
+            form.style.display = 'block';
+            btn.textContent = "フォームを閉じる";
+        }
+    });
 });
